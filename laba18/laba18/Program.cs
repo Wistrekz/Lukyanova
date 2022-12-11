154c872fdf28cf1827076c9022ed8de00292015b
Wistrekz <96283348+Wistrekz@users.noreply.github.com>
2022-12-11 18:30:46 +0300

    Add files via upload

---
+++ laba18/laba18/Program.cs
@@ -0,0 +1,19 @@
+﻿using laba18;
+
+auto p1 = new auto { name = "Лада", number = 34 };
+auto p2 = new auto { name = "Рено", number = 23 };
+auto p3 = new auto { name = "форд", number = 21 };
+
+auto[] people = new auto[] { p1, p2, p3 };
+if(p1.CompareTo(p2) == 1)                 // Б.Е.С.П.О.Л.Е.З.Н.О.
+{
+    Array.Sort(people);
+}
+
+
+foreach (auto p in people)
+{
+    Console.WriteLine($"{p.name} - {p.number}");
+}
+Console.ReadKey();
+
---
+++ laba18/laba18/auto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba18
+{
+    internal class auto : IComparable
+    {
+        public string name { get; set; }
+        public int number { get; set; }
+
+        public int CompareTo(object? o)
+        {
+            auto p = o as auto;
+            return p.name.CompareTo(p.number.ToString());
+        }
+
+    }
+}
