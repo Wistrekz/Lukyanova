d1c5d4e92534b24673a71534076ab4417da400e7
Wistrekz <96283348+Wistrekz@users.noreply.github.com>
2022-12-12 20:48:25 +0300

    Add files via upload

---
+++ laba19/laba19/Dancing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba19
+{
+    public struct Dancing
+    {
+        public string Imya;
+        public string Familiya;
+        public string Otchestvo;
+        public string gorod;
+        public string familiya_trenera;
+        public int ocenka_za_tanec;
+        public int unite_ball;
+
+    }
+
+}
---
+++ laba19/laba19/Program.cs
@@ -0,0 +1,45 @@
+﻿using laba19;
+
+try
+{
+    Console.WriteLine("Сколько всего клиентов?");
+    int client_number = Convert.ToInt32(Console.ReadLine());
+    Dancing[] client = new Dancing[client_number]; //массив объектов
+
+    for (int q = 0; q < client_number; q++)
+    {
+        Console.WriteLine($"Участник №{q + 1}");
+        Console.WriteLine("Имя:");
+        client[q].Imya = Console.ReadLine();
+        Console.WriteLine("Фамилия:");
+        client[q].Familiya = Console.ReadLine();
+        Console.WriteLine("Отчество: ");
+        client[q].Otchestvo = Console.ReadLine();
+        Console.WriteLine("Город: ");
+        client[q].gorod = Console.ReadLine();
+        Console.WriteLine("Фамилия тренера: ");
+        client[q].familiya_trenera = Console.ReadLine();
+        Console.WriteLine("Оценка за танец: ");
+        client[q].ocenka_za_tanec = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Общий балл:");
+        client[q].unite_ball = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("Сортировка по баллу:");
+    var _allStatInfo = client;
+    var sortedList = _allStatInfo.OrderBy(si => si.Familiya).Reverse().ToList(); //сортировка через спец. интерфейс по фамилии
+    sortedList = sortedList.OrderBy(si => si.unite_ball).Reverse().ToList(); //сортировка через спец. интерфейс по баллу
+
+    foreach (var o in sortedList)
+    {
+        Console.WriteLine("Клиент: " + o.Imya + " " + o.Familiya + " " + o.Otchestvo + ";" + Environment.NewLine + "Тренер: " + o.familiya_trenera + ", Балл: " + o.unite_ball);
+        Console.WriteLine();
+    }
+
+    Console.ReadKey();
+}
+catch(FormatException) // ошибка формата
+{
+    Console.WriteLine("Ошибка. Вы ввели не тот формат данных. Здесь могут быть только числа");
+}
+
+
